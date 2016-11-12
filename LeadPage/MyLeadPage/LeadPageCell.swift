3061e1bdb16fbd2498408f928424e0a130485250
Chaser-Z <18330570306@163.com>
2016-11-12 16:02:34 +0800

    first commit

---
+++ LeadPage/MyLeadPage/LeadPageCell.swift
@@ -0,0 +1,13 @@
+//
+//  LeadPageCell.swift
+//  LeadPage
+//
+//  Created by yeeaoo on 16/6/27.
+//  Copyright © 2016年 枫韵海. All rights reserved.
+//
+
+import UIKit
+
+class LeadPageCell: UICollectionViewCell {
+    
+}
